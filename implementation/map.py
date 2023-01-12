70c31029d2e0aab92fa590fe1410af0b76325aca
Taejun Kim <tjkimin2021@gmail.com>
2023-01-12 14:29:09 +0900

    2

---
+++ implementation/map.py
@@ -0,0 +1,22 @@
+# N을 입력받기
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+
+# L, R, U, D에 따른 이동 방향
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+# 이동 계획을 하나씩 확인
+for plan in plans:
+  # 이동 후 좌표 구하기
+  for i in range(len(move_types)):
+    if plan == move_types[i]:
+      nx = x + dx[i]
+      ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+      continue
+    x, y = nx, ny
+
+print(x, y)
