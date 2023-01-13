a5118973dce0c9be7859503a3fa321bebd81557a
Taejun Kim <tjkimin2021@gmail.com>
2023-01-13 13:35:41 +0900

    DFSBFS

---
+++ implementation/movegame2.py
@@ -0,0 +1,46 @@
+n, m = map(int, input().split())
+
+d = [[0] * m for _ in range(n)]
+x, y, direction = map(int, input().split())
+d[x][y] = 1
+
+array = []
+for i in range(n):
+  array.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left():
+  global direction
+  direction -= 1
+  if direction == -1:
+    direction = 3
+
+count = 1
+turn_time = 0
+while True:
+  turn_left()
+  nx = x + dx[direction]
+  ny = y + dy[direction]
+  if d[nx][ny] == 0 and array[nx][ny] == 0:
+    # d[nx][ny]는 회전하고 나서 한 칸 앞에 있는 땅을 방문하지 않았다는 뜻이고, array[nx][ny]는 그것이 육지라는 뜻이다.
+    d[nx][ny] = 1
+    x = nx
+    y = ny
+    count += 1
+    turn_time = 0
+    continue
+  else:
+    turn_time += 1
+
+  if turn_time == 4:
+    nx = x - dx[direction]
+    ny = y - dx[direction]
+    if array[nx][ny] == 0:
+      x = nx
+      y = ny
+    else:
+      break
+    turn_time = 0
+    
